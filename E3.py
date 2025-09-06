dd5427b1cbba311366afefd43196a17264e55ed3
Dr-Verdin <piscioneri24122004@gmail.com>
2025-09-06 19:32:23 -0300

    E3 teste

---
+++ E3.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+# Transformar a string de horas em minutos
+def h_para_min(h_str):
+    h, min = map(int, h_str.split(":"))
+    return (h * 60) + min
+
+# Número de casos testes
+n_testes = int(input())
+
+for _ in range(n_testes):
+    n_modelos = int(input())
+    n_alugueis = int(input())
+
+    dados = []
+    for _ in range(n_alugueis):
+        line = input().split()
+        id = int(line[0])
+        h_inicio = h_para_min(line[1])
+        h_fim = h_para_min(line[2])
+        modelo = int(line[3])
+        dados.append([id, h_inicio, h_fim, modelo])
+    
+    # Agrupar os alugueis por modelo
+    dados_modelo = defaultdict(list)
+    for d in dados:
+        dados_modelo[d[3]].append(d)
+
+    saidas = [] # para impressão
+
+    # Para cada modelo
+    for modelo in sorted(dados_modelo.keys()):
+        alugueis = dados_modelo[modelo]
+        alugueis.sort(key=lambda x: (x[2], x[1]))   # ordenar por fim ed eposi por inicio
+
+        fim_ultimo_aluguel = -1 # fim do ultimo aluguel escolhido
+        escolhidos = []     # alugueis escolhidos
+        for a in alugueis:
+            if a[1] >= fim_ultimo_aluguel:
+                escolhidos.append(a[0])
+                fim_ultimo_aluguel = a[2]
+        
+        saidas.append(f"{modelo}: {len(escolhidos)} = {', '.join(map(str, escolhidos))}")   # saída de cada modelo
+    
+    print(" | ".join(saidas))   # saida completa do teste
