9896c9c9530e71c3232a080116af09e0b5ecb0f7
Dr-Verdin <piscioneri24122004@gmail.com>
2025-10-05 11:17:44 -0300

    saida ok

--- E5.py
+++ E5.py
@@ -1,15 +1,30 @@
+# FUnção para calcular o número de ultrapassagens
+def calc_ultrapassagens(local_jogadores):
+    #  desenvolver ...
+    return total_ultrapassagens
 
 # Número de trechos
-nTrechos = int(input())
+n_trechos = int(input())
+ultrapassagens_trecho = []                # Armazena os trechos com a quantidade de ultrapassagens
 
-for _ in range(nTrechos):
+for trecho in range(n_trechos):
     # Número de jogadores
-    nPlayersTrecho = int(input())
-    localPlayers = []
+    n_jogadores_trecho = int(input())
+    local_jogadores = []
 
     # Posição dos jogadores
-    for _ in range(nPlayersTrecho):
+    for _ in range(n_jogadores_trecho):
         inicio, final = input().split()
-        localPlayers.append([inicio, final])
+        local_jogadores.append([inicio, final])
 
-    
+    # Calculo do número de ultrapassagens
+    total_ultrapassagens = calc_ultrapassagens(local_jogadores)
+
+    ultrapassagens_trecho.append([trecho, total_ultrapassagens])
+
+# Ordenar os trechos de acordo com o número de ultrapassagens
+ultrapassagens_trecho.sort(key=lambda x: x[1], reverse=True)
+
+# Imprimir resultado
+for trecho, n_ultrapassagens in ultrapassagens_trecho:
+    print(trecho, n_ultrapassagens)
